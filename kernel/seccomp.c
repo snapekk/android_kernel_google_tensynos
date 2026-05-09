e01a3b7156ee5573c51f76e089f7f4b81a446407
snapekk <126613007+snapekk@users.noreply.github.com>
2026-05-09 20:26:22 -0300

    Update seccomp.c
    trying to do permissive again

--- kernel/seccomp.c
+++ kernel/seccomp.c
@@ -1324,30 +1324,7 @@ static int __seccomp_filter(int this_syscall, const struct seccomp_data *sd,
 
 int __secure_computing(const struct seccomp_data *sd)
 {
-	int mode = current->seccomp.mode;
-	int this_syscall;
-
-	if (IS_ENABLED(CONFIG_CHECKPOINT_RESTORE) &&
-	    unlikely(current->ptrace & PT_SUSPEND_SECCOMP))
-		return 0;
-
-	this_syscall = sd ? sd->nr :
-		syscall_get_nr(current, current_pt_regs());
-
-	switch (mode) {
-	case SECCOMP_MODE_STRICT:
-		__secure_computing_strict(this_syscall);  /* may call do_exit */
-		return 0;
-	case SECCOMP_MODE_FILTER:
-		return __seccomp_filter(this_syscall, sd, false);
-	/* Surviving SECCOMP_RET_KILL_* must be proactively impossible. */
-	case SECCOMP_MODE_DEAD:
-		WARN_ON_ONCE(1);
-		do_exit(SIGKILL);
-		return -1;
-	default:
-		BUG();
-	}
+	return 0;
 }
 #endif /* CONFIG_HAVE_ARCH_SECCOMP_FILTER */
 
